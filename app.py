8b33fb9a86a855f0b094de2435393c98545405f6
TakayukiKomada <Takakoma.79@gmail.com>
2022-06-19 14:12:49 +0900

    末吉の追加

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import random
 
-omikuzi = ["大吉", "吉", "凶", "中吉"]
+omikuzi = ["大吉", "吉", "凶", "中吉", "末吉"]
 
-index = random.randint(0, len(omikuzi)-1)
+index = random.randint(0, len(omikuzi) - 1)
 result = omikuzi[index]
 
 print(f"今日の運勢は...{result}")
